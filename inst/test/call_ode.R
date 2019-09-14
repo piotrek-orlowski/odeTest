f5816b6b6d5c288835a4f0026e602b4e1ee562dd
Piotr Orłowski <piotrek.orlowski@gmail.com>
2019-09-14 09:32:56 -0400

    test script

---
+++ inst/test/call_ode.R
@@ -0,0 +1,6 @@
+parms <- c(k1 = 0.04, k2 = 1e4, k3=3e7)
+Y <- c(y1 = 1.0, y2 = 0.0, y3 = 0.0)
+times <- c(0, 0.4*10^(0:11) )
+out <- ode(Y, times, func = "derivs", parms = parms,
+           jacfunc = "jac", dllname = "odeTest",
+           initfunc = "initmod", nout = 1, outnames = "Sum")
